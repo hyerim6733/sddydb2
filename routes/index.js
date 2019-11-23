750c1b9b1cf35ad7b21e1e248c4ad7ea3a8c0be0
rim <hyerim6733@naver.com>
2019-11-23 16:16:06 +0900

    main

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "ㅆㄷㄷㅇ" });
 });
 
+router.get("/main", function(req, res, next) {
+  res.render("main", { title: "ㅆㄷㄷㅇ" });
+});
+
 module.exports = router;
