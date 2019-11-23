4c70b6caec4b260119a90921d7b867373a29c312
rim <hyerim6733@naver.com>
2019-11-23 15:15:24 +0900

    ㅆㄷㄷㅇ

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "써도대여" });
+  res.render("index", { title: "ㅆㄷㄷㅇ" });
 });
 
 module.exports = router;
